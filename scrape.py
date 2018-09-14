669e37df60320cd27e6051070037a9b43d0ce2ab
Austin Carr <aucarr@microsoft.com>
2018-09-14 13:11:49 -0500

    json to excel - working

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 from requests_html import HTMLSession
 from pandas.io.json import json_normalize
 
@@ -7,10 +8,28 @@ from pandas.io.json import json_normalize
 s = HTMLSession()
 
 # use search URL to use as referer and get api
-s.headers.update({'referer': 'https://turo.com/search?country=US&defaultZoomLevel=11&endDate=09%2F05%2F2018&endTime=10%3A00&international=true&isMapSearch=false&itemsPerPage=200&latitude=32.7766642&location=Dallas&locationType=City&longitude=-96.79698789999999&maximumDistanceInMiles=30&region=TX&sortType=RELEVANCE&startDate=08%2F30%2F2018&startTime=10%3A00'})
-r = s.get('https://turo.com/api/search?country=US&defaultZoomLevel=11&endDate=09%2F05%2F2018&endTime=10%3A00&international=true&isMapSearch=false&itemsPerPage=200&latitude=32.7766642&location=Dallas&locationType=City&longitude=-96.79698789999999&maximumDistanceInMiles=30&region=TX&sortType=RELEVANCE&startDate=08%2F30%2F2018&startTime=10%3A00')
+s.headers.update({'referer': 'https://turo.com/search?country=US&defaultZoomLevel=11&endDate=09%2F11%2F2018&endTime=10%3A00&international=true&isMapSearch=false&itemsPerPage=200&latitude=32.7766642&location=dallas&locationType=City&longitude=-96.79698789999999&maximumDistanceInMiles=30&region=TX&sortType=RELEVANCE&startDate=09%2F04%2F2018&startTime=10%3A00&type=6'})
+r = s.get('https://turo.com/api/search?country=US&defaultZoomLevel=11&endDate=09%2F11%2F2018&endTime=10%3A00&international=true&isMapSearch=false&itemsPerPage=200&latitude=32.7766642&location=dallas&locationType=City&longitude=-96.79698789999999&maximumDistanceInMiles=30&region=TX&sortType=RELEVANCE&startDate=09%2F04%2F2018&startTime=10%3A00&type=6')
+#print(r.content)
 
 # jsonify the response and print some json
+
 j = r.json()
-#print(j['list'][0])
-print(json.dumps(r['list'][0]['reviewCount'], indent=2))
+#print(j['list'])
+#print(json.dumps(j['list'][0]['reviewCount'], indent=2))
+
+# Iterate through the list of entries and export to CSV
+i = 0
+max = range(150,200)
+for i in max:
+	car_data = j['list'][i]
+	#print(car_data)
+	with open('filecars.csv', 'a') as car_csv:  
+		w = csv.DictWriter(car_csv, car_data.keys())
+		#w.writeheader()
+		w.writerow(car_data)
+# create the csv writer object
+#	with open('file.csv', 'w') as car_csv:  
+#		w = csv.DictWriter(car_csv, car_data.keys())
+#		w.writeheader()
+#		w.writerow(car_data)
